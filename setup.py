6b812a96eac635745f63e47ed033ab8815d7946c
idmillington <idmillington@googlemail.com>
2015-12-25 10:07:25 +0000

    Imported package from private repo.

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Argparse Visual Types',
+    'description': 'Paper sizes and manipulations',
     'author': 'Ian Millington',
-    'url': 'https://github.com/idmillington/argvtypes',
-    'download_url': 'https://github.com/idmillington/argvtypes',
+    'url': 'https://github.com/idmillington/papersizes',
+    'download_url': 'https://github.com/idmillington/papersizes',
     'author_email': 'idmillington@gmail.com',
     'version': '1.0',
     'install_requires': ['nose'],
-    'packages': ['argvtypes'],
+    'packages': ['papersizes'],
     'scripts': [],
-    'name': 'argvtypes'
+    'name': 'papersizes'
 }
 
 setup(**config)
